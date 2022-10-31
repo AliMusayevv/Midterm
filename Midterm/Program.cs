b07f21d43589c27021f68ffaec4203d5c24a26a5
AliMusayevv <emusayev001@gmail.com>
2022-10-31 00:41:48 -0800

    Add project files.

---
+++ Midterm/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Girilən 10 rəqəm içindən 10 ve 20 arasındaki rəqəmlerin sayı və cəmini tapmaq.
+             */
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + 1 + "- ci Reqemi daxil edin: ");
+
+                int imput = Int32.Parse(Console.ReadLine());
+                arr[i] = imput;
+            }
+
+            int say = 0;
+            int cem = 0;
+            foreach (int a in arr)
+            {
+                if (a <= 20 && a >= 10)
+                {
+
+
+                    cem = cem + a;
+                    say++;
+
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("10 ve 20 Aralığındaki rəqəm sayı : " + say);
+            Console.WriteLine("10 ve 20 Aralığındaki rəqəmlərin cəmi :" + cem);
+
+
+            Console.Read();
+
+        }
+    }
+}
